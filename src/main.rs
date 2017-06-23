cbaa8f8cb7536bfe68c19380197bc984d7a6d8a8
Luke Chen Shui <chenshuiluke@gmail.com>
2017-06-22 23:54:26 -0500

    Added separator b/w the git outputs and further refactored the code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate github_rs;
 extern crate serde_json;
 extern crate hyper;
-use serde_json::{Value, Error};
+extern crate terminal_size;
+use serde_json::Value;
 use github_rs::client::Github;
 use std::process::Command;
+use terminal_size::{Width, Height, terminal_size};
 
 fn get_nth_argument(n:usize) -> Result<String, &'static str>{
     for arg in std::env::args().skip(n){
@@ -12,33 +14,41 @@ fn get_nth_argument(n:usize) -> Result<String, &'static str>{
     Err("No token was provided")
 }
 
+fn get_width_of_terminal() -> usize{
+    let size = terminal_size();
+    if let Some((Width(w),Height(h))) = size {
+        return w as usize;
+    }
+    0 as usize
+}
+
 fn process_repo_json(repo_json:Option<Value>, token:String){
     if let Some(json) = repo_json{
-        if(json.is_array()){
-            println!("Is array!");
-            let repo_vec = json.as_array();
-            println!("Array: {:?}", repo_vec);
-            for repo_item in repo_vec{
-                for repo_attribute in repo_item{
-                    if(repo_attribute.is_object()){
-                        if let Some(html_obj) = repo_attribute.get("html_url"){
+        if json.is_array() {
+            if let Some(repository_list) = json.as_array(){
+                for repository in repository_list{
+                    //println!("Repository: {:?}", repository);
+                    if(repository.is_object()){
+                        if let Some(html_obj) = repository.get("html_url"){
                             if let Some(html) = html_obj.as_str(){
                                 println!("Downloading {}", html);
+                                
                                 let protocol:String = html.chars().take(8).collect();
                                 let url:String = html.chars().skip(8).collect();
                                 let clone_url = format!("{}{}@{}",protocol,token,url);
-                                Command::new("git")
+                                let result = Command::new("git")
                                     .arg("clone")
                                     .arg(clone_url)
                                     .spawn()
                                     .expect("failed to execute command")
                                     .wait();
+                                println!("{}", "-".repeat(get_width_of_terminal()));
                             }
                             
                         } 
                     }
-                }
 
+                }
             }
         }
     }      
