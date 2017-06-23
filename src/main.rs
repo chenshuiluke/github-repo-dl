b7e659477ff113dd4cce58332500d51ba6d626de
Luke Chen Shui <chenshuiluke@gmail.com>
2017-06-22 22:49:06 -0500

    Further refactored the code

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,44 @@ extern crate hyper;
 use serde_json::{Value, Error};
 use github_rs::client::Github;
 use std::process::Command;
+
 fn get_nth_argument(n:usize) -> Result<String, &'static str>{
     for arg in std::env::args().skip(n){
         return Ok(arg);
     }
     Err("No token was provided")
+}
+
+fn process_repo_json(repo_json:Option<Value>, token:String){
+    if let Some(json) = repo_json{
+        if(json.is_array()){
+            println!("Is array!");
+            let repo_vec = json.as_array();
+            println!("Array: {:?}", repo_vec);
+            for repo_item in repo_vec{
+                for repo_attribute in repo_item{
+                    if(repo_attribute.is_object()){
+                        if let Some(html_obj) = repo_attribute.get("html_url"){
+                            if let Some(html) = html_obj.as_str(){
+                                println!("Downloading {}", html);
+                                let protocol:String = html.chars().take(8).collect();
+                                let url:String = html.chars().skip(8).collect();
+                                let clone_url = format!("{}{}@{}",protocol,token,url);
+                                Command::new("git")
+                                    .arg("clone")
+                                    .arg(clone_url)
+                                    .spawn()
+                                    .expect("failed to execute command")
+                                    .wait();
+                            }
+                            
+                        } 
+                    }
+                }
+
+            }
+        }
+    }      
 }
 
 fn process_repos(repos:Result<(hyper::Headers, hyper::StatusCode, std::option::Option<Value>), github_rs::errors::Error>,
@@ -20,35 +53,7 @@ fn process_repos(repos:Result<(hyper::Headers, hyper::StatusCode, std::option::O
                     println!("Invalid token entered!");
                 }
                 _ => {
-                    if let Some(json) = json{
-                        if(json.is_array()){
-                            println!("Is array!");
-                            let repo_vec = json.as_array();
-                            println!("Array: {:?}", repo_vec);
-                            for repo_item in repo_vec{
-                                for repo_attribute in repo_item{
-                                    if(repo_attribute.is_object()){
-                                        if let Some(html_obj) = repo_attribute.get("html_url"){
-                                            if let Some(html) = html_obj.as_str(){
-                                                println!("Downloading {}", html);
-                                                let protocol:String = html.chars().take(8).collect();
-                                                let url:String = html.chars().skip(8).collect();
-                                                let clone_url = format!("{}{}@{}",protocol,token,url);
-                                                Command::new("git")
-                                                    .arg("clone")
-                                                    .arg(clone_url)
-                                                    .spawn()
-                                                    .expect("failed to execute command")
-                                                    .wait();
-                                            }
-                                            
-                                        } 
-                                    }
-                                }
-
-                            }
-                        }
-                    }                            
+                    process_repo_json(json,token);                      
                 }
             }
 
