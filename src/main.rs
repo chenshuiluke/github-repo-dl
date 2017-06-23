25011bbe0b1eff7335a86b2c10c2a96f7a99f305
Luke Chen Shui <chenshuiluke@gmail.com>
2017-06-23 15:35:33 -0500

    Refactored the code even more

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,34 @@ extern crate terminal_size;
 use serde_json::Value;
 use github_rs::client::Github;
 use std::process::Command;
-use terminal_size::{Width, Height, terminal_size};
+use terminal_size::{Width, terminal_size};
+
+struct Config{
+    token:String,
+    output_location:String,
+}
+
+impl Config{
+    pub fn new() -> Config{
+        Config {
+            token: String::new(),
+            output_location: String::from("repositories")
+        }
+    }
+    pub fn new_with_token(token:String) -> Config{
+        let mut config:Config = Config::new();
+        config.token = token;
+        config
+    }
+    pub fn new_with_token_and_output_dir(token:String, output_location:String) -> Config{
+        let mut config:Config = Config::new();
+        config.token = token;
+        config.output_location = output_location;
+        config
+    }
+
+}
+
 
 fn get_nth_argument(n:usize) -> Result<String, &'static str>{
     for arg in std::env::args().skip(n){
@@ -16,36 +43,36 @@ fn get_nth_argument(n:usize) -> Result<String, &'static str>{
 
 fn get_width_of_terminal() -> usize{
     let size = terminal_size();
-    if let Some((Width(w),Height(h))) = size {
+    if let Some((Width(w),_)) = size {
         return w as usize;
     }
     0 as usize
 }
 
-fn process_repo_json(repo_json:Option<Value>, token:String){
+fn process_repo_json(repo_json:Option<Value>, config:Config){
     if let Some(json) = repo_json{
         if json.is_array() {
             if let Some(repository_list) = json.as_array(){
                 for repository in repository_list{
                     //println!("Repository: {:?}", repository);
-                    if(repository.is_object()){
-                        if let Some(html_obj) = repository.get("html_url"){
-                            if let Some(html) = html_obj.as_str(){
-                                println!("Downloading {}", html);
-                                
-                                let protocol:String = html.chars().take(8).collect();
-                                let url:String = html.chars().skip(8).collect();
-                                let clone_url = format!("{}{}@{}",protocol,token,url);
-                                let result = Command::new("git")
-                                    .arg("clone")
-                                    .arg(clone_url)
-                                    .spawn()
-                                    .expect("failed to execute command")
-                                    .wait();
-                                println!("{}", "-".repeat(get_width_of_terminal()));
-                            }
-                            
-                        } 
+                    if repository.is_object(){
+
+                        let repository_html_url = repository.get("html_url").unwrap().as_str().unwrap();
+                        let repository_name = repository.get("name").unwrap().as_str().unwrap();
+                        println!("Downloading {}", repository_html_url);
+                        
+                        let protocol:String = repository_html_url.chars().take(8).collect();
+                        let url:String = repository_html_url.chars().skip(8).collect();
+                        let clone_url = format!("{}{}@{}",protocol,config.token,url);
+                        let result = Command::new("git")
+                            .arg("clone")
+                            .arg(clone_url)
+                            .arg(format!("{}/{}", &config.output_location, repository_name))
+                            .spawn()
+                            .expect("failed to execute command")
+                            .wait();
+                        println!("{:?}", result);
+                        println!("{}", "-".repeat(get_width_of_terminal()));
                     }
 
                 }
@@ -55,15 +82,15 @@ fn process_repo_json(repo_json:Option<Value>, token:String){
 }
 
 fn process_repos(repos:Result<(hyper::Headers, hyper::StatusCode, std::option::Option<Value>), github_rs::errors::Error>,
-    token:String){
+    config:Config){
     match repos {
-        Ok((headers, status, json)) => {
+        Ok((_, status, json)) => {
             match status{
                 hyper::StatusCode::Unauthorized => {
                     println!("Invalid token entered!");
                 }
                 _ => {
-                    process_repo_json(json,token);                      
+                    process_repo_json(json,config);                      
                 }
             }
 
@@ -73,17 +100,21 @@ fn process_repos(repos:Result<(hyper::Headers, hyper::StatusCode, std::option::O
 }
 
 fn main() {
-    
-    match get_nth_argument(1){
-        Ok(tok) => {
-            let token:String = tok;
-            let client = Github::new(&token).unwrap();
-            let repos = client.get()
-                        .user()
-                        .repos()
-                        .execute();
-          process_repos(repos,token);
+    let arg_tuple=(get_nth_argument(1), get_nth_argument(2));
+    let config:Config;
+    //println!("{:?}", arg_tuple);
+    match arg_tuple{
+        (Ok(tok), Ok(dir)) => config=Config::new_with_token_and_output_dir(tok, dir),
+        (Ok(tok), _) => config=Config::new_with_token(tok),
+        _ => {
+            println!("Token was not supplied!");
+            std::process::exit(1);
         }
-        Err(e) => println!("{}", e)
     }
+    let client = Github::new(&config.token).unwrap();
+    let repos = client.get()
+                .user()
+                .repos()
+                .execute();
+    process_repos(repos,config);
 }
